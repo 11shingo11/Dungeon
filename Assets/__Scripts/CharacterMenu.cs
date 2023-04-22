a4461215143de3561e99c417b1671c9417106b05
11shingo11 <shin2312@icloud.com>
2023-04-22 22:10:40 +0300

    update

--- Assets/__Scripts/CharacterMenu.cs
+++ Assets/__Scripts/CharacterMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class CharacterMenu : MonoBehaviour
 {
     //textb fields
-    public Text levelText, hitPointText, pososText, upgradeCostText, xpText;
+    public Text levelText, hitPointText, pesosText, upgradeCostText, xpText;
 
     //Logic
     private int currentCharacterSelection = 0;
@@ -15,7 +15,54 @@ public class CharacterMenu : MonoBehaviour
     public RectTransform xpBar;
 
     //Character Selection
-    public void OnArrowClick
+    public void OnArrowClick(bool right)
+    {
+        if (right)
+        {
+            currentCharacterSelection++;
+            //if we went too far away
+            if (currentCharacterSelection == GameManager.instance.playerSprites.Count)
+            {
+                currentCharacterSelection = 0;
+            }
+            OnSelectionChanged();
+        }
+        else
+        {
+            currentCharacterSelection--;
+            //if we went too far away
+            if (currentCharacterSelection < 0)
+            {
+                currentCharacterSelection = GameManager.instance.playerSprites.Count - 1;
+            }
+            OnSelectionChanged();
+        }
+    }
+    private void OnSelectionChanged()
+    {
+        characterSelectionSprite.sprite = GameManager.instance.playerSprites[currentCharacterSelection];
+    }
 
+    //weapon upgrade
+    public void OnUpdateClick()
+    {
+        //
+    }
+
+    //Upgrade the character info
+    public void UpdateMenu()
+    {
+        //weapon
+        weaponSprite.sprite = GameManager.instance.weaponSprites[0];
+        upgradeCostText.text = "NOT IMPLEMENTED";
+        //meta
+        levelText.text = "NOT IMPLEMENTED";
+        hitPointText.text = GameManager.instance.player.hitpoint.ToString();
+        pesosText.text = GameManager.instance.pesos.ToString();
+        
+        //xp Bar
+        xpText.text = "NOT IMPLEMENTED";
+        xpBar.localScale = new Vector3 (0.5f, 0, 0);
+    }
 
 }
